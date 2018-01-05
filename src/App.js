4cdee530415522bbfb11e623a1e63eee09c2df98
Cristian <cri.gacitua.flores@gmail.com>
2018-01-04 18:56:54 -0800

    configuracion de redux

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//permite acceder al estado como la posibilidad de ejecutar acciones (dispatch) a nuestros componentes (ACTIONS y ACTION CREATOR)
+import { connect } from 'react-redux';
+
 class App extends Component {
   render() {
     return (
@@ -11,6 +14,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
+          {this.props.information}
+          <br/>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
@@ -18,4 +23,26 @@ class App extends Component {
   }
 }
 
-export default App;
+//Ingresa como props a nuestro Component tanto el STATE como los DISPATCH
+
+//internamente hace una susbscripcion y un get state
+//por lo que constantemente en caso de un cambio en el STATE se actualiza
+//o se ejecuta nuevamente
+const mapStateToProps = (state) => {
+  //retorna un objeto javascript
+  return {
+    information: state.cantidad
+  }
+}
+
+//
+const mapDispatchToProps = {
+  aumentar: () => {
+    return { type: "AUM" }
+  },
+  disminuir: () => {
+    return { type: "DIS" }
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,5 +4,25 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//redux
+import { createStore } from 'redux';
+//react-redux
+//Provider: engloba el componente principal para que los demas componentes hijos tengan acceso al store
+import { Provider } from 'react-redux';
+
+const state = { cantidad: 2 };
+
+const reducer = (state, action) => {
+    return state;
+}
+
+//crear store
+const store = createStore(reducer, state);
+
+ReactDOM.render(
+    //1. Implementar el PROVIDER
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
